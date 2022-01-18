c14c8c4824c3970b73c978b537b6cac2d4c91553
WalukaUI <cwaluka@yahoo.com>
2022-01-18 04:01:12 -0800

    images added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,6 @@
-import react, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import startsound from "./start.wav";
+import stopsound from "./stop.wav";
 
 export default function Main() {
   let ranNum = Math.floor(Math.random() * 40);
@@ -6,16 +8,34 @@ export default function Main() {
 
   const [isstart, setIsStart] = useState(false);
   const [seconds, setSeconds] = useState(ransec);
+  const [isZero, setIsZero] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  function reset() {
-    setSeconds(0);
-    setIsStart(false);
+  let startAudio = new Audio(startsound);
+  let stopAudio = new Audio(stopsound);
+
+  function start(e) {
+    e.preventDefault();
+    setIsPlaying(true);
+    setIsZero(false);
+    setIsStart(true);
+    setSeconds(ransec);
+    startAudio.play();
   }
 
+  function playAudio() {
+    stopAudio.play();
+  }
+  function reset(e) {
+    e.preventDefault();
+    setIsPlaying(false);
+  }
   useEffect(() => {
     let interval = null;
     if (seconds === 0) {
-      alert("gggg");
+      setIsStart(false);
+      setIsZero(true);
+      playAudio();
     } else if (isstart) {
       interval = setInterval(() => {
         setSeconds((seconds) => seconds - 1);
@@ -28,11 +48,22 @@ export default function Main() {
 
   return (
     <div>
-      <button onClick={() => setIsStart(!isstart)}>START</button>
+      <button onClick={start}>START</button>
 
       <h2>{seconds}</h2>
-
-      <button onClick={() => reset}>RESET</button>
+      {isPlaying ? (
+        <img
+          alt="stopnstart"
+          src={
+            !isZero
+              ? "https://media.giphy.com/media/67ThRZlYBvibtdF9JH/giphy.gif"
+              : "https://media.giphy.com/media/EKDGlXqALQUOQ/giphy.gif"
+          }
+        />
+      ) : (
+        ""
+      )}
+      <button onClick={reset}>RESET</button>
     </div>
   );
 }
